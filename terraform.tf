1cc371c9944e72555fb7b7197e656757b7544a6b
Devopsncloudtraning <devopsncloudtraning@gmail.com>
2025-09-28 18:16:10 +0530

    Initial commit: Terraform EKS cluster setup
    Add Terraform configuration to provision an EKS cluster on AWS, including VPC, managed node groups, IAM roles, and required providers. Includes .gitignore, license, outputs, and variables files for project setup.

---
+++ terraform.tf
@@ -0,0 +1,44 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  # cloud {
+  #   workspaces {
+  #     name = "learn-terraform-eks"
+  #   }
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+
+    backend "s3" {
+      bucket = "tf-state-file-4-az-pipeline-eks" # Will be overridden from build
+      key    = "terraform.tfstate" # Will be overridden from build
+      region = "us-east-1"
+    }
+
+
+  }
+
+  required_version = "~> 1.3"
+}
+
